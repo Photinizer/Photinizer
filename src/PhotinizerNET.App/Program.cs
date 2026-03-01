79addd1638f43c55bc8f391f270bbcd416785564
Anton <yoyo1991@yandex.ru>
2026-03-02 02:33:31 +0300

    simplify Program.cs

--- src/PhotinizerNET.App/Program.cs
+++ src/PhotinizerNET.App/Program.cs
@@ -1,32 +1,26 @@
-﻿using PhotinizerNET.UI.Own;
-//using PhotinizerNET.UI.Vue; //requires project reference to PhotinizerNET.UI.Vue project
+﻿using PhotinizerNET;
+using PhotinizerNET.UI.Own;
 
-namespace PhotinizerNET.App;
 
-static class Program
-{
-    [STAThread]
-    static void Main(string[] args)
-        => new Photinizer()
-        .AddOwnUI(Path.Combine("Frontend","components"))/*or AddVueJs() */
-        .Run(setup: o =>
+new Photinizer()
+    .AddOwnUI(Path.Combine("Frontend", "components"))
+    .Run(setup: o =>
+    {
+        // for example
+        o.Window.SetDevToolsEnabled(true);
+
+        o.Messenger
+            .OnQuery("Hello, backend!", _ => "Hello, frontend!")
+            .OnTask("save username", data => File.WriteAllText("data.dat", data.GetProperty("username").ToString()))
+            .OnTask("delete username", _ => File.Delete("data.dat"))
+            .OnQuery("get username", _ => File.Exists("data.dat") ? File.ReadAllText("data.dat") : "dear friend");
+
+        Task.Run(async () =>
         {
-            // for example
-            o.Window.SetDevToolsEnabled(true);
-
-            o.Messenger
-                .OnQuery("Hello, backend!", _ => "Hello, frontend!")
-                .OnTask("save username", data => File.WriteAllText("data.dat", data.GetProperty("username").ToString()))
-                .OnTask("delete username", _ => File.Delete("data.dat"))
-                .OnQuery("get username", _ => File.Exists("data.dat") ? File.ReadAllText("data.dat") : "dear friend");
-
-            Task.Run(async () =>
+            while (true)
             {
-                while (true)
-                {
-                    await Task.Delay(1000);
-                    await o.Messenger.SendTask("update timer", DateTime.Now.ToString("HH:mm:ss"));
-                }
-            });
+                await Task.Delay(1000);
+                await o.Messenger.SendTask("update timer", DateTime.Now.ToString("HH:mm:ss"));
+            }
         });
-}
+    });
--- src/PhotinizerNET/Photinizer.cs
+++ src/PhotinizerNET/Photinizer.cs
@@ -1,5 +1,6 @@
 ﻿using PhotinizerNET.Core.Settings;
 using PhotinizerNET.Exceptions;
+using System.Runtime.InteropServices;
 
 namespace PhotinizerNET;
 
@@ -19,9 +20,28 @@ public class Photinizer
         }
         else
         {
-            var app = new PhotinizedApp(settings);
-            setup?.Invoke(app);
-            app.Run();
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                RunIsSTAThread(() => RunApp(settings, setup));
+            else
+                RunApp(settings, setup);
         }
+    }
+
+    private void RunApp(PhotinizerSettings settings, Action<PhotinizedApp> setup = null)
+    {
+        var app = new PhotinizedApp(settings);
+        setup?.Invoke(app);
+        app.Run();
+    }
+
+    private void RunIsSTAThread(ThreadStart threadStart)
+    {
+        Thread newThread = new(threadStart);
+#pragma warning disable CA1416 // Only for Windows (already checked above)
+        newThread.SetApartmentState(ApartmentState.STA);
+#pragma warning restore CA1416 // Only for Windows (already checked above)
+        newThread.Start();
+        newThread.Join();
     }
 }
