684f3a167f97dd9e866b64c72ccc0e588422ccb8
ivanvoyager <5630565+ivanvoyager@users.noreply.github.com>
2026-03-05 22:52:39 +0300

    Add properties to IPhotinizerConfiguration

--- src/Photinizer/Abstractions/IPhotinizerConfiguration.cs
+++ src/Photinizer/Abstractions/IPhotinizerConfiguration.cs
@@ -1,4 +1,6 @@
-﻿using Photinizer.Messaging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Photinizer.Messaging;
 using Photino.NET;
 
 namespace Photinizer;
@@ -8,4 +10,7 @@ public interface IPhotinizerConfiguration
     PhotinoWindow MainWindow { get; }
     Messenger Messenger { get; }
     IServiceProvider Services { get; }
+    IConfiguration Configuration { get; }
+    IAppEnvironment Environment { get; }
+    ILogger Logger { get; }
 }
