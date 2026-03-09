c5dfbdd50f8ab93ad394c454d9d95f32e1975c61
Anton <yoyo1991@yandex.ru>
2026-03-09 07:50:42 +0300

    Fluent switcher

--- samples/Photinizer.OwnUI.Minimal/Program.cs
+++ samples/Photinizer.OwnUI.Minimal/Program.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.Extensions.Logging;
+﻿#define FLUENT_
+using Microsoft.Extensions.Logging;
 using Photinizer.Builder;
 using Photinizer.Desktop;
 using Photinizer.OwnUI.Minimal.Backend.Extensions;
 using Photinizer.UI.Own;
 
+#if !FLUENT
 // Minimal API style:
 var builder = Application.CreateBuilder(args);
 builder.UseOwnUI();
@@ -17,15 +19,16 @@ app.MapQuery("Hello, backend!", _ => "Hello, frontend!");
 await app.RunAllServices();
 app.Run();
 
-
+#else
 // Fluent API Style:
-//Application
-//    .Create(b => b
-//        .AddOwnUI()
-//        .Logging.ClearProviders().AddConsole()
-//        .Services.AddSampleServices())
-//    .Run(config: o =>
-//    {
-//        o.Messenger.OnQuery("Hello, backend!", _ => "Hello, frontend!");
-//        // await app.RunAllServices(); /// TODO: make it work for Fluent, maybe o.UseAllServices()..
-//    });
+Application
+    .Create(b => b
+        .AddOwnUI()
+        .Logging.ClearProviders().AddConsole()
+        .Services.AddSampleServices())
+    .Run(config: o =>
+    {
+        o.Messenger.OnQuery("Hello, backend!", _ => "Hello, frontend!");
+        // await app.RunAllServices(); /// TODO: make it work for Fluent, maybe o.UseAllServices()..
+    });
+#endif
